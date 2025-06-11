cebdd852c44362bb7269c0ab471de77f5b13fd0e
Chakka Vijay Chandra <chakkavijay415@gmail.com>
2025-06-11 21:44:37 +0530

    feat: add GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/modern-blog-app',
+  assetPrefix: '/modern-blog-app/',
+}
 
 module.exports = nextConfig
